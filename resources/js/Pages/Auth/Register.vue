7a1812bbd807b8232ec69ab59977707799fd3bf7
zloykotuk <7loykotuk@gmail.com>
2025-04-08 11:49:44 +0300

    Change user profile page (#18) (#27)

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -2,7 +2,7 @@
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import InputError from "@/Components/UI/Forms/InputError.vue";
 import InputLabel from "@/Components/UI/Forms/InputLabel.vue";
-import PrimaryButton from "@/Components/UI/Forms/PrimaryButton.vue";
+import PrimaryButton from "@/Components/UI/Button/PrimaryButton.vue";
 import TextInput from "@/Components/UI/Forms/TextInput.vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
 
