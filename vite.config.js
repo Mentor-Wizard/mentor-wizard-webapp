07b64405569ac4e69b465adea89a98495b55c99f
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2025-04-08 11:55:40 +0300

    feat: remove unused resolve aliases from Vite config

--- vite.config.js
+++ vite.config.js
@@ -25,12 +25,6 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    resolve: {
-        alias: {
-            ziggy: '/vendor/tightenco/ziggy/dist/vue.m',
-            '@': '/resources/js',
-        },
-    },
     server: {
         https: false,
         host: '0.0.0.0',
