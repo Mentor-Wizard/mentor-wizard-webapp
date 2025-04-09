e8538afe3aa6df1baa47a12237a2c65881e60888
Asafailo <82404189+Asafailo@users.noreply.github.com>
2025-04-09 11:55:36 +0300

    #8698627z4 adding footer (#22)
    Co-authored-by: vadym <vadimasaf2003@gmail.com>
    Co-authored-by: Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,6 +1,7 @@
 <script setup>
 import ApplicationLogo from '@/Components/UI/Logo/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
+import Footer from "@/Components/Navigation/Footer.vue";
 </script>
 
 <template>
@@ -18,5 +19,6 @@ import { Link } from '@inertiajs/vue3';
         >
             <slot />
         </div>
+            <Footer></Footer>
     </div>
 </template>
