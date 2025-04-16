b9538f40084be10ea458db716820e8889758f744
Aleksandr Burhela <7loykotuk@gmail.com>
2025-04-16 22:49:38 +0300

    Fixed code style (#39)

--- tests/Feature/Pages/ErrorPageTest.php
+++ tests/Feature/Pages/ErrorPageTest.php
@@ -1,12 +1,14 @@
 <?php
 
 declare(strict_types=1);
+
+use Illuminate\Testing\Fluent\AssertableJson;
 use Inertia\Testing\AssertableInertia as Assert;
 
-it('Visited an unregistered page', function () {
+it('Visited an unregistered page', function (): void {
     $response = $this->get('/page-not-found');
 
-    $response->assertInertia(fn (Assert $page) => $page
+    $response->assertInertia(fn (Assert $page): AssertableJson => $page
         ->component('Error')
         ->whereAll([
             'status' => 404,
