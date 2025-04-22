e6b31964c6994145661b168ebb12b0a6ea214beb
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2025-04-22 19:19:58 +0300

    feat: remove unnecessary 'page' data from Inertia requests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -49,9 +49,6 @@ class HandleInertiaRequests extends Middleware
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
-            'page'    => [
-                'uri' => url()->current(),
-            ],
             'project' => [
                 'name' => config('app.name'),
             ],
