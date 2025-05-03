3be7f2fd8b9209cf908cecdfded00ce9aee5d773
Aleksandr Burhela <7loykotuk@gmail.com>
2025-05-03 22:44:39 +0300

    Changed numbers to constants (#39)

--- tests/Feature/Pages/ErrorPageTest.php
+++ tests/Feature/Pages/ErrorPageTest.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 use App\Models\User;
 use Database\Seeders\RoleSeeder;
+use Symfony\Component\HttpFoundation\Response;
 
 it('Visited an unregistered page', function (): void {
     $response = $this->get('/page-not-found');
 
-    $response->assertStatus(404)
-        ->assertSee(404)
+    $response->assertStatus(Response::HTTP_NOT_FOUND)
+        ->assertSee(Response::HTTP_NOT_FOUND)
         ->assertSee('Not Found');
 });
 
@@ -18,7 +19,8 @@ it('Visited site in maintenance mode', function (): void {
 
     $response = $this->get('/');
 
-    $response->assertStatus(503)
+    $response->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE)
+        ->assertSee(Response::HTTP_SERVICE_UNAVAILABLE)
         ->assertSee('Service Unavailable');
 
     Artisan::call('up');
@@ -35,6 +37,7 @@ it('Spam requests to url', function (): void {
         $response = $this->actingAs($user)->post('email/verification-notification');
     }
 
-    $response->assertStatus(429)
+    $response->assertStatus(Response::HTTP_TOO_MANY_REQUESTS)
+        ->assertSee(Response::HTTP_TOO_MANY_REQUESTS)
         ->assertSee('Too Many Requests');
 });
