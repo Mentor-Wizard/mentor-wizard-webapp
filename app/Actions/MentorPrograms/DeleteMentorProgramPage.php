520736160f504a6b062422f5d5ca63b38511ae23
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2025-06-26 12:52:25 +0300

    feat: refactor mentor program deletion logic, add tests and route policy checks

--- app/Actions/MentorPrograms/DeleteMentorProgramPage.php
+++ app/Actions/MentorPrograms/DeleteMentorProgramPage.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace App\Actions\MentorPrograms;
 
 use App\Models\MentorProgram;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Lorisleiva\Actions\Concerns\AsController;
-use Symfony\Component\HttpFoundation\Response;
 
 class DeleteMentorProgramPage
 {
@@ -17,10 +15,6 @@ class DeleteMentorProgramPage
 
     public function handle(Request $request, MentorProgram $mentorProgram): RedirectResponse
     {
-        throw_unless($mentorProgram->exists, new ModelNotFoundException('Mentor program not found.'));
-
-        abort_if($request->user()->cannot('delete', $mentorProgram), Response::HTTP_FORBIDDEN, 'Unauthorized action.');
-
         $mentorProgram->delete();
 
         return redirect()->route('mentor-program.create');
