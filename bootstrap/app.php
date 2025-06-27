136fe0349cee4adfa4e54dc3a2b4a29a92602f15
vadym <vadimasaf2003@gmail.com>
2025-06-27 23:36:22 +0300

    Remove obsolete UserResource and middleware aliases.
    The UserResource and related middleware aliases were no longer in use and have been removed to clean up the codebase. This simplifies the application and eliminates unused code.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,12 +24,6 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
-        $middleware->alias([
-            'role'               => Spatie\Permission\Middleware\RoleMiddleware::class,
-            'permission'         => Spatie\Permission\Middleware\PermissionMiddleware::class,
-            'role_or_permission' => Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
-        ]);
-
         //        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {})->create();
