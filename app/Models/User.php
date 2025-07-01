d46c66b949453af75b7fbd183e15e91aee089b4e
basil_str <basil.str@gmail.com>
2025-07-01 21:00:13 +0300

    feat: install filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,6 @@ use Spatie\Permission\Traits\HasRoles;
 /**
  * @property-read UserProfile $profile
  * @property string $username
- *
  * @mixin IdeHelperUser
  */
 #[ObservedBy(UserObserver::class)]
