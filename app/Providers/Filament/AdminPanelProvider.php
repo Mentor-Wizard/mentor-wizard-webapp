5b012fc4bffd703bc389ad5ccb600d8472a618bb
basil_str <basil.str@gmail.com>
2025-07-02 12:44:02 +0300

    fix: code review

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,8 +27,8 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->id('admin')
-            ->path('admin')
+            ->id('supervisor')
+            ->path('supervisor')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,11 +2,16 @@
 
 declare(strict_types=1);
 
+$telescopeProviders = app()->environment('local', 'testing', 'ci') && class_exists(Laravel\Telescope\TelescopeServiceProvider::class)
+    ? [
+        Laravel\Telescope\TelescopeServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
+    ] : [];
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
-    Laravel\Telescope\TelescopeServiceProvider::class,
     Olssonm\VeryBasicAuth\VeryBasicAuthServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
+    ...$telescopeProviders,
 ];
