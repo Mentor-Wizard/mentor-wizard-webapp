c93454cc0cdedd1ba6174f417caf1e1240ebbffb
basil_str <basil.str@gmail.com>
2025-07-02 11:13:28 +0300

    feat: install panels

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,16 +1,10 @@
 <?php
 
-declare(strict_types=1);
-
-$telescopeProviders = app()->environment('local', 'testing', 'ci') && class_exists(Laravel\Telescope\TelescopeServiceProvider::class)
-    ? [
-        Laravel\Telescope\TelescopeServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
-    ] : [];
-
 return [
     App\Providers\AppServiceProvider::class,
-    Spatie\Permission\PermissionServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+    Laravel\Telescope\TelescopeServiceProvider::class,
     Olssonm\VeryBasicAuth\VeryBasicAuthServiceProvider::class,
-    ...$telescopeProviders,
+    Spatie\Permission\PermissionServiceProvider::class,
 ];
