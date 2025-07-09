4e6dc9d55ac1e5f66e261636b41bc2b14bfebf9a
basil_str <basil.str@gmail.com>
2025-07-09 12:33:26 +0300

    feat: add universal paginator

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ use Spatie\Permission\Traits\HasRoles;
 /**
  * @property-read UserProfile $profile
  * @property string $username
+ *
  * @mixin IdeHelperUser
  */
 #[ObservedBy(UserObserver::class)]
