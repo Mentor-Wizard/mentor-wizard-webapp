ae55d113eaa27f95f92b4f385ebef9e9eb6b6580
kennwat <slevin.lucky@ukr.net>
2025-08-23 10:28:38 +0000

    refactor: move getFilamentName method to the correct position in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -136,16 +136,16 @@ class User extends Authenticatable implements HasMedia, HasName, MustVerifyEmail
         return $this->hasMany(Chat::class, 'coach_id');
     }
 
+    public function getFilamentName(): string
+    {
+        return $this->username ?? '';
+    }
+
     protected function rating(): Attribute
     {
         return Attribute::make(
             get: fn (): float => (float) $this->mentorReviews()->avg('rating'),
         );
-    }
-
-    public function getFilamentName(): string
-    {
-        return $this->username ?? '';
     }
 
     /**
