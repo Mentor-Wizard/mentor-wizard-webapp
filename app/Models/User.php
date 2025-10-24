afea4c236f9feceeac7871c7f17231e8ccdd154f
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2025-10-24 14:33:24 +0300

    Documentation updates and fixes (#95)

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,7 @@ class User extends Authenticatable implements HasMedia, HasName, MustVerifyEmail
         'username',
         'email',
         'password',
+        'preferences',
     ];
 
     /**
