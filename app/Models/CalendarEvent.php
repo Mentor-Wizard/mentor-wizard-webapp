2b76df62940cdd4060f926df581cd02f875a2662
vadym <vadimasaf2003@gmail.com>
2026-02-03 10:20:14 +0200

    chore: remove unused `status` cast from `CalendarEvent` model

--- app/Models/CalendarEvent.php
+++ app/Models/CalendarEvent.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Enums\CalendarEventStatusEnum;
 use App\Observers\CalendarEventObserver;
 use App\Policies\CalendarEventPolicy;
 use Database\Factories\CalendarEventFactory;
