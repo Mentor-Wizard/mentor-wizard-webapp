25df87edf42662c9fbb17930782a703c8598f455
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2026-02-25 12:10:04 +0200

    feat: Update Redis configuration in .env.ci and database.php for improved clarity

--- config/database.php
+++ config/database.php
@@ -153,7 +153,7 @@ return [
             'host'     => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
-            'port'     => env('REDIS_PORT', '6379'),
+            'port'     => (int) env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', '0'),
         ],
 
@@ -162,7 +162,7 @@ return [
             'host'     => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
-            'port'     => env('REDIS_PORT', '6379'),
+            'port'     => (int) env('REDIS_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
     ],
