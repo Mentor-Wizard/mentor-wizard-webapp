e59997861ce64ed298779479ad17edca14db0a49
Oleksii Kruhlyk <oleksii.kruhlyk@gmail.com>
2026-05-09 13:36:51 +0300

    refactor: add type annotations in SendMessage and update phpstan config
    - Introduce type annotations for companion chat in `SendMessage` to improve code clarity.
    - Remove obsolete error suppression from `phpstan.neon`.

--- app/Actions/Chat/SendMessage.php
+++ app/Actions/Chat/SendMessage.php
@@ -13,6 +13,7 @@ use App\Models\Chat;
 use App\Models\ChatMessage;
 use App\Models\User;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\UploadedFile;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -35,6 +36,7 @@ class SendMessage
     {
         $user = $request->user();
         $companion = $chat->companion($user);
+        /** @var Chat&object{pivot: Pivot&object{status: string, is_muted: bool}} $companionChat */
         $companionChat = $companion->chats()
             ->wherePivot('chat_id', $chat->getKey())
             ->first();
